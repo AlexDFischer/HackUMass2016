09bac0f98729a4971cbc3c4ed975ace3383f74fd
Will <willzhang100@gmail.com>
2016-10-08 23:01:09 -0400

    added stuff

--- src/Board.java
+++ src/Board.java
@@ -1,6 +1,14 @@
 
 public class Board {
 
+	private static int BOARD_DIMENSION = 500; 
+	
+	//Is this useful? Remove it if it isn't
+	public static double[] convertRadiansToMM(double pitch, double roll) {
+		double[] heights = new double[4];
+		return heights;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
--- src/BoardController.java
+++ src/BoardController.java
@@ -1,9 +1,44 @@
+import java.io.IOException;
 
-public class BoardController {
+import com.leapmotion.leap.*;
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
 
+class BoardController {
+
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		MotionListener listener = new MotionListener();
+		Controller controller = new Controller();
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter a number"); //STOPPED HERE
+		
+		controller.addListener(listener);
+		
+		System.out.println("Press enter to quit...");
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 	}
-
 }
+
+class MotionListener extends Listener {
+	
+	public void onConnect(Controller controller) {
+        controller.enableGesture(Gesture.Type.TYPE_SWIPE);
+    }
+
+    public void onFrame(Controller controller) {
+    	Frame frame = controller.frame();
+        HandList hands = frame.hands();
+        Hand hand = hands.get(0);
+        float pitch = hand.direction().pitch();
+        float roll = hand.palmNormal().roll();
+    }
+}
+
+
